b05b5ec27c82c62d1be59744470a8b026d6b1aac
Guernsey <ngy@batty.ornl.gov>
2018-02-07 13:25:23 -0500

    File explorer lists close upon one item selected, but persists on n > 1.

--- src/components/FileExplorer/FitList/FitList.vue
+++ src/components/FileExplorer/FitList/FitList.vue
@@ -1,13 +1,15 @@
 <template>
     <v-select label='Fit' 
-        :items='filesToFit'
-        v-model='selected'
-        autocomplete
-        chips
-        deletable-chips
-        hint='Pick a file to fit'
-        max-height='150'
-        :disabled='!filesToFit.length'>
+      ref='fitList'
+      :items='filesToFit'
+      v-model='selected'
+      autocomplete
+      chips
+      deletable-chips
+      hint='Pick a file to fit'
+      max-height='250'
+      :disabled='!filesToFit.length'
+      @change='$refs.fitList.isActive = $event.length > 1'>
     </v-select>
 </template>
 
