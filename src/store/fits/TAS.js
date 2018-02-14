13ff96c78cda6455cdb29b38a3b6171f0a0b16a2
Guernsey <ngy@batty.ornl.gov>
2018-02-14 13:43:25 -0500

    Updated TAS fit initial guesses.

--- src/store/fits/TAS.js
+++ src/store/fits/TAS.js
@@ -20,8 +20,11 @@ export default {
     equation: `(A*exp(${(-4 * Math.log(2)).toFixed(3)}*(x-center)^2/fwhm^2))`,
     initialValues: [
       { coefficient: 'A', value: 'max(y) - min(y)', constant: false },
-      { coefficient: 'center', value: 'sum(x * y) / sum(y)', constant: false },
-      { coefficient: 'fwhm', value: `${2 * Math.sqrt(2 * Math.log(2))} * std(x)`, constant: false },
+      { coefficient: 'center', value: 'sum(dotMultiply(x, y)) / sum(y)', constant: false },
+      { coefficient: 'fwhm',
+        value: `sqrt(abs(sum(dotMultiply(dotPow(x - (sum(dotMultiply(x, y)) / sum(y)), 2), y))/ sum(y))) * ${Math.sqrt(8 * Math.log(2))}`,
+        constant: false,
+      },
     ],
     transformations: {
       x: 'x',
@@ -33,12 +36,24 @@ export default {
     note: '',
   },
   Lorentzian: {
+    /*
+      Initial Guess for  A, center, fwhm from the y_noise!
+      A = np.max(y_noise) - np.min(y_noise)
+      center = np.sum(x*y_noise)/np.sum(y_noise)
+      sigma = np.sqrt(np.abs(
+          np.sum((x-center)**2 * y_noise)/np.sum(y_noise)
+      ))
+      fwhm = 2 * sigma
+    */
     title: 'Lorentzian',
     equation: '(A * ((fwhm/2) / ((x-center)^2 + (fwhm/2)^2)))',
     initialValues: [
       { coefficient: 'A', value: 'max(y) - min(y)', constant: false },
-      { coefficient: 'fwhm', value: `${2 * Math.sqrt(2 * Math.log(2))} * std(x)`, constant: false },
-      { coefficient: 'center', value: 'sum(x * y) / sum(y)', constant: false },
+      { coefficient: 'fwhm',
+        value: '2 * sqrt(abs(sum(dotMultiply(dotPow((x - sum(dotMultiply(x, y)) / sum(y)), 2), y)) / sum(y)))',
+        constant: false,
+      },
+      { coefficient: 'center', value: 'sum(dotMultiply(x, y)) / sum(y)', constant: false },
     ],
     transformations: {
       x: 'x',
