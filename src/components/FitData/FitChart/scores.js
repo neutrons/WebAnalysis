baf7b6731380fd9447d48f58ba2bf001666fe2c1
Guernsey <ngy@batty.ornl.gov>
2018-02-16 13:53:30 -0500

    Removed commented code from scores.js

--- src/components/FitData/FitChart/scores.js
+++ src/components/FitData/FitChart/scores.js
@@ -1,36 +1,5 @@
 import LM from 'ml-levenberg-marquardt';
 
-// function linearFit(m, b) {
-//   return t => (m * t) + b;
-// }
-
-// const options = {
-//   damping: 0.01,
-//   initialValues: [1, 3],
-//   gradientDifference: 10e-2,
-//   maxIterations: 100,
-//   errorTolerance: 10e-3,
-// };
-
-// const data = {
-//   x: [
-//     0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045,
-//     0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09,
-//     0.095, 0.1, 0.105, 0.11, 0.115, 0.12, 0.125, 0.13, 0.135,
-//     0.14, 0.145, 0.15, 0.155, 0.16, 0.165, 0.17, 0.175,
-//   ],
-//   y: [
-//     1.059053, 0.820483, 0.889606, 0.695512, 0.578699, 0.527058, 0.541055,
-//     0.585198, 0.484689, 0.513663, 0.412635, 0.380731, 0.351293, 0.324131,
-//     0.29907, 0.271001, 0.25461, 0.234924, 0.21676, 0.216298, 0.184536,
-//     0.177404, 0.145856, 0.124078, 0.121299, 0.123407, 0.126478, 0.110142,
-//     0.096938, 0.091407, 0.086616, 0.076146, 0.072392, 0.071292, 0.060748,
-//   ],
-// };
-
-
-// const result = LM(data, linearFit, options);
-
 /**
  * Return the correlation coefficient of determination (r) and chi-square.
  * From: https://github.com/mljs/regression-base
@@ -79,6 +48,3 @@ export default function scores(x, y, func) {
     rmsd: (rmsd * rmsd) / n,
   };
 }
-
-// console.log(scores(data.x, data.y,
-//   linearFit(result.parameterValues[0], result.parameterValues[1])));
