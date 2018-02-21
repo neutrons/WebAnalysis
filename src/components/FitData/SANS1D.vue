112b98b86531876b8d30bee7873e06c142a1aad8
Guernsey <ngy@batty.ornl.gov>
2018-02-21 10:45:24 -0500

    Fit results tab is activated when curve is fit.

--- src/components/FitData/SANS1D.vue
+++ src/components/FitData/SANS1D.vue
@@ -1,65 +1,64 @@
 <template>
-    <div>
-      <keep-alive>
-        <v-chart ID='SANS-Fit'>
-          <template slot-scope='props' slot='tabs-slot'>
+<div>
+  <keep-alive>
+    <v-chart ID='SANS-Fit'>
+      <template slot-scope='props' slot='tabs-slot'>
 
-            <v-slide-y-transition>
-                <v-flex xs12>
-                  <div v-show='props.showTabs'>
-                    <v-tabs v-model='props.activeParentTab'>
+        <v-slide-y-transition>
+          <v-flex xs12 v-show='props.showTabs'>
+            <v-tabs v-model='props.activeParentTab'>
 
-                        <v-tabs-bar v-if='props.fileToFit'>
-                            <v-tabs-item href='#tab-fit' ripple v-if='props.fileToFit'>Fit</v-tabs-item>
-                            <v-tabs-slider color='accent'></v-tabs-slider>
-                        </v-tabs-bar>
+              <v-tabs-bar>
+                <v-tabs-item href='#tab-fit' ripple v-if='props.fileToFit'>Fit</v-tabs-item>
+                <v-tabs-slider color='accent'></v-tabs-slider>
+              </v-tabs-bar>
 
-                        <!-- nested tab items  -->
-                        <v-tabs-items>
-                          <!-- fit results tab content -->
-                          <v-tabs-content id='tab-fit' ripple v-if='props.fileToFit'>
-                            <v-tabs>
-                              <v-tabs-bar>
-                                <v-tabs-slider color='accent'></v-tabs-slider>
-                                <v-tabs-item href='fit-results'>
-                                  Fit Results
-                                </v-tabs-item>
+              <!-- nested tab items  -->
+              <v-tabs-items>
+                <!-- fit results tab content -->
+                <v-tabs-content id='tab-fit' ripple v-if='props.fileToFit'>
+                  <v-tabs>
+                    <v-tabs-bar>
+                      <v-tabs-slider color='accent'></v-tabs-slider>
+                      <v-tabs-item href='fit-results'>
+                        Fit Results
+                      </v-tabs-item>
 
-                                <v-tabs-item href='fit-data'>
-                                  Fitted Data
-                                </v-tabs-item>
-                              </v-tabs-bar>
+                      <v-tabs-item href='fit-data'>
+                        Fitted Data
+                      </v-tabs-item>
+                    </v-tabs-bar>
 
-                              <v-tabs-items>
-                                <v-tabs-content id='fit-results'>
-                                    <v-card>
-                                      <v-card-text class='tab-card-text'>
-                                        <v-fit-results-table :x-scale='props.xScale'></v-fit-results-table>
-                                      </v-card-text>
-                                    </v-card>
-                                </v-tabs-content>
+                    <v-tabs-items>
+                      <v-tabs-content id='fit-results'>
+                        <v-card>
+                          <v-card-text class='tab-card-text'>
+                            <v-fit-results-table :x-scale='props.xScale'></v-fit-results-table>
+                          </v-card-text>
+                        </v-card>
+                      </v-tabs-content>
 
-                                <v-tabs-content id='fit-data'>
-                                  <v-card>
-                                    <v-card-text class='tab-card-text'>
-                                      <v-fitted-data-table :fitted-data='props.fittedData' :file-to-fit='props.fileToFit'></v-fitted-data-table>
-                                    </v-card-text>
-                                  </v-card>
-                                </v-tabs-content>
-                                </v-tabs-items>
-                            </v-tabs>
-                          </v-tabs-content>
-                        </v-tabs-items>
-                    <!-- end of nested tab items -->
-                    </v-tabs>
-                  </div>
-                </v-flex>
-              </v-slide-y-transition>
-          
-          </template>
-        </v-chart>
-      </keep-alive>
-    </div>
+                      <v-tabs-content id='fit-data'>
+                        <v-card>
+                          <v-card-text class='tab-card-text'>
+                            <v-fitted-data-table :fitted-data='props.fittedData' :file-to-fit='props.fileToFit'></v-fitted-data-table>
+                          </v-card-text>
+                        </v-card>
+                      </v-tabs-content>
+                    </v-tabs-items>
+                  </v-tabs>
+                </v-tabs-content>
+              </v-tabs-items>
+              <!-- end of nested tab items -->
+            </v-tabs>
+          </v-flex>
+        </v-slide-y-transition>
+
+      </template>
+    </v-chart>
+  </keep-alive>
+</div>
+
 </template>
 
 <script>
