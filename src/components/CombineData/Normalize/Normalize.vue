98041c7bccec4e58dc9739e40f8e20888c34a282
Guernsey <ngy@batty.ornl.gov>
2018-02-26 16:02:01 -0500

    Added validation to combine inputs.

--- src/components/CombineData/Normalize/Normalize.vue
+++ src/components/CombineData/Normalize/Normalize.vue
@@ -6,7 +6,6 @@
     chips
     v-model='editNormalizeField'
     :items='normalizeOptions'
-    
   ></v-select>
 
   <v-text-field 
@@ -14,15 +13,36 @@
     type='number'
     v-model.number='editNormalizeValue'
     :step='defaultSettings.normalize.increment'
+    required
+    :rules='[checkValue]'
   ></v-text-field>
 
-  <v-btn outline block flat color='success' @click='normalizeData' :disabled='isNormalized'>Normalize Data</v-btn>
-  <v-btn outline block flat color='warning' @click='resetNormalizeData' :disabled='!isNormalized'>Reset Data</v-btn>
+  <v-btn outline block flat color='success' @click='normalizeData' :disabled='isNormalized || !valid'>Normalize Data</v-btn>
+  <v-btn outline block flat color='warning' @click='resetNormalizeData' :disabled='!isNormalized || !valid'>Reset Data</v-btn>
 </div>
 </template>
 
 <script>
 export default {
   name: 'Normalize',
+  data() {
+    return {
+      valid: true,
+    };
+  },
+  methods: {
+    checkValue(value) {
+      if (typeof value !== 'number') {
+        this.valid = false;
+        return 'Must be a number';
+      } else if (value === 0) {
+        this.valid = false;
+        return 'Must not equal 0. Divide by zero.';
+      }
+
+      this.valid = true;
+      return true;
+    },
+  },
 };
 </script>
