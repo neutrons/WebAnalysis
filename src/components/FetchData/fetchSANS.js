189c9cee87f6dd8016c62488892c7f3c38e3d66d
Guernsey <ngy@batty.ornl.gov>
2018-04-27 10:06:29 -0400

    Fixed SANS2D not fetching when on SANS1D tabs.

--- src/components/FetchData/fetchSANS.js
+++ src/components/FetchData/fetchSANS.js
@@ -3,7 +3,24 @@ import { eventBus } from '../../assets/js/eventBus';
 export default {
   methods: {
     fetchSANS(data, silenceMessage = false) {
-      const temp = {};
+      const vm = this;
+      const fetchFiles = {
+        SANS1D: {},
+        SANS2D: {},
+      };
+
+      function addDataFile(group, content) {
+        const filename = content.filename;
+        fetchFiles[group][filename] = content;
+      }
+
+      function sendData(namespace, payload) {
+        vm.$store.dispatch(`${namespace}/addFetchFiles`, payload)
+          .then(() => {
+            // Notify that fetch was a success
+            if (silenceMessage !== true) eventBus.$emit('add-notification', 'Data fetched.', 'success');
+          });
+      }
 
       data.forEach((el) => {
         const jobTitle = el.job_title;
@@ -11,32 +28,43 @@ export default {
 
         el.results.forEach((r) => {
           const key = r.type;
+
           // use regex to match filename from url
           const matchFilename = r.url.match(/([^\/]+)\./); // eslint-disable-line
           const filename = matchFilename[0].replace(/\.+$/, '');
 
-          if (key === this.$route.meta.subgroup) {
-            temp[filename] = {
+          // check if key matches 1D or 2D and add file content
+          if (['SANS1D', 'SANS2D'].indexOf(key) > -1) {
+            const content = {
               filename,
+              jobTitle,
               id: r.id,
               url: r.url,
-              jobTitle,
               dateModified: jobModified,
               tags: [jobTitle, 'fetched'],
               loadType: 'fetched',
             };
+
+            addDataFile(key, content);
           }
         });
       });
 
-      const namespace = this.$route.name !== 'SANS2D' ? 'SANS' : 'SANS/SANS2D';
-      if (Object.keys(temp).length > 0) {
-        this.$store.dispatch(`${namespace}/addFetchFiles`, temp)
-          .then(() => {
-            // Notify that fetch was a success
-            if (silenceMessage !== true) eventBus.$emit('add-notification', 'Data fetched.', 'success');
-          });
-      } else {
+      const isSANS1D = Object.keys(fetchFiles.SANS1D).length > 0;
+      const isSANS2D = Object.keys(fetchFiles.SANS2D).length > 0;
+
+      // if there is sans1d data trigger action to add it
+      if (isSANS1D) {
+        sendData('SANS', fetchFiles.SANS1D);
+      }
+
+      // if there is sans2d data trigger action to add it
+      if (isSANS2D) {
+        sendData('SANS/SANS2D', fetchFiles.SANS2D);
+      }
+
+      // If both are empty emit message
+      if (!isSANS1D && !isSANS2D) {
         eventBus.$emit('add-notification', 'No data to fetch.', 'warning');
       }
     },
