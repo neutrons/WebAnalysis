c8f183769bda04fe085e04f3c2247caeccaf205d
Guernsey <ngy@batty.ornl.gov>
2018-05-04 16:47:13 -0400

    Fixed images to appear in documentation.

--- src/components/Documentation/Documentation.vue
+++ src/components/Documentation/Documentation.vue
@@ -7,6 +7,15 @@
 
     <v-flex xs12 v-for='(section, sectionTitle) in chapter.sections' :key='section.id'>
       <h3>{{ sectionTitle }}</h3>
+      
+      <p>{{ section.info }}</p>
+      
+      <figure v-if='section.image !== false'>
+        <img :alt='sectionTitle' :src='section.image'>
+        <figcaption>
+          {{ section.imageCaption }}
+        </figcaption>
+      </figure>
 
       <div v-html='section.content' class='mb-3'></div>
       <span :id='section.id'></span>
@@ -27,112 +36,71 @@ export default {
       title: 'Welcome to documentation!',
       guide: {
         Basics: {
+          // disabling eslint because it doesn't like using the require() statement
+          /* eslint-disable */
           sections: {
             'App Layout': {
               id: 'basics-app-layout',
-              content: `
-                </div>
-                  <figure>
-                    <img alt='Plot Layout Image' src='../../static/AppLayout.png'>
-                    <figcaption>
-                      <p><sup><b>(1)</b></sup> Left side of the page is the control panel. You can select files to plot, fit, or combine. You can change plot scales, tag files, along with other controls specific to feature.</p>
-                      <p><sup><b>(2)</b></sup> Middle of page is the plot view. For the top half, users can view the plot or a table of the data being plotted. Immediately below the chart is a toolbar of tools to alter and edit the plot. The bottom half contains additional information related to plot features, such as fit results, file metadata, etc.</p>
-                      <p><sup><b>(3)</b></sup> Top page of the page users can Fetch and Upload data, as well as navigate between plot features Browse, Graph, and etc..</p>
-                      <p><sup><b>(4)</b></sup> Bottom of page holds the footer which you can change colors themes of the application.</p>
-                    </figcaption>
-                  </figure>
-                <div>
-                `,
+              info: '',
+              image: require('../../assets/images/AppLayout.png'),
+              imageCaption: `(1) Left side of the page is the control panel. You can select files to plot, fit, or combine. You can change plot scales, tag files, along with other controls specific to feature.
+(2) Middle of page is the plot view. For the top half, users can view the plot or a table of the data being plotted. Immediately below the chart is a toolbar of tools to alter and edit the plot. The bottom half contains additional information related to plot features, such as fit results, file metadata, etc.
+(3) Top page of the page users can Fetch and Upload data, as well as navigate between plot features Browse, Graph, and etc..
+(4) Bottom of page holds the footer which you can change colors themes of the application.`,
             },
             'Fetch Data': {
               id: 'basics-fetch-data',
-              content: `
-                <div>
-                  <figure>
-                    <img alt='Fetch Data Image' src='../../static/FetchData.gif'>
-                    <figcaption>Click to fetch data from server</figcaption>
-                  </figure>
-                </div>`,
+              info: '',
+              image: require('../../assets/images/FetchData.gif'),
+              imageCaption: 'Click to fetch data from server',
             },
-            'Upload Data': {
+            'Click Upload Data': {
               id: 'basics-upload-data',
-              content: `
-                <div>
-                  <figure>
-                    <img alt='Click to Upload Image' src='../../static/UploadFile.gif'>
-                    <figcaption>Click to upload a file</figcaption>
-                  </figure>
-                  <br>
-                  <p style='text-align: center;'><b>OR</b></p>
-                  <br>
-                  <figure>
-                    <img alt='Drag and Drop Image' src='../../static/DragDrop.gif'>
-                    <figcaption>Drag and drop files to upload</figcaption>
-                  </figure>
-                </div>`,
+              info: '',
+              image: require('../../assets/images/UploadFile.gif'),
+              imageCaption: 'Click to upload a file',
+            },
+            'Drag Upload Data': {
+              id: 'basics-drag-upload-data',
+              info: '',
+              image: require('../../assets/images/DragDrop.gif'),
+              imageCaption: 'Click to upload a file',
             },
             'Change Scale': {
               id: 'basics-change-scales',
-              content: `
-                <div>
-                  <p>
-                    Use the Scales panel in the left sidebar to change the current plot's scales...gif goes here.
-                  </p>
-
-                  <img alt='Change Plot Scales Image' src='../../static/ChangeScales.gif'>
-                </div>`,
+              info: `Use the Scales panel in the left sidebar to change the current plot's scales...gif goes here.`,
+              image: require('../../assets/images/ChangeScales.gif'),
+              imageCaption: '',
             },
             'Delete Points': {
               id: 'basics-delete-points',
-              content: `
-                <div>
-                  <p>
-                    If you identify an outlier in the plot, simply click the point to delete it. A prompt will be displayed to confirm.
-                  </p>
-
-                  <img alt='Delete Points Image' src='../../static/DeletePoint.gif'>
-                </div>`,
+              info: 'If you identify an outlier in the plot, simply click the point to delete it. A prompt will be displayed to confirm.',
+              image: require('../../assets/images/DeletePoint.gif'),
+              imageCaption: '',
             },
             'Toggle Plot Elements': {
               id: 'basics-toggle-plot-elements',
-              content: `
-                <div>
-                  <p>
-                    If you don't need scatter lines, error bars, points, or legends for a given plot, simply toggle the elements off with the button labeled 'Toggle Plot Elements' below the plot view. If you notice the plot is sluggish while zooming and panning, one culprit is too many svg elements on the plot; toggling off plot elements can help improve slow zooming.
-                  </p>
-                  <img alt='Toggle Plot Elements Image' src='../../static/TogglingElements.gif'>
-                </div>`,
+              info: `If you don't need scatter lines, error bars, points, or legends for a given plot, simply toggle the elements off with the button labeled 'Toggle Plot Elements' below the plot view. If you notice the plot is sluggish while zooming and panning, one culprit is too many svg elements on the plot; toggling off plot elements can help improve slow zooming.`,
+              image: require('../../assets/images/TogglingElements.gif'),
+              imageCaption: '',
             },
             'Edit Chart': {
               id: 'basics-edit-chart',
-              content: `
-                <div>
-                  <p>If you want to edit the current plot, click the 'Edit Chart Button' below the plot. This will take you to a new page where you can customize the appearance of the plot using the Plotly package.</p>
-
-                  <img alt='Edit Plot Image' src='../../static/EditChart.gif'>
-                </div>`,
+              info: `If you want to edit the current plot, click the 'Edit Chart Button' below the plot. This will take you to a new page where you can customize the appearance of the plot using the Plotly package.`,
+              image: require('../../assets/images/EditChart.gif'),
+              imageCaption: '',
             },
             'Change Theme': {
               id: 'basics-change-theme',
-              content: `
-                <div>
-                  <p>
-                    If you want to change the color of the app's appearance, click one of the three options at the bottom right of the page.
-                  </p>
-
-                  <img alt='Change Color Theme Image' src='../../static/ChangeTheme.gif'>
-                </div>`,
+              info: `If you want to change the color of the app's appearance, click one of the three options at the bottom right of the page.`,
+              image: require('../../assets/images/ChangeTheme.gif'),
+              imageCaption: '',
             },
             'Unzoom Chart': {
               id: 'basics-unzoom-chart',
-              content: `
-                <div>
-                  <p>
-                    If you have zoomed or panned a plot and would like to go to its default state, simply click the 'Unzoom Plot' button below the plot view.
-                  </p>
-
-                  <img alt='Unzoom Plot Image' src='../../static/UnzoomPlot.gif'>
-                </div>`,
+              info: `If you have zoomed or panned a plot and would like to go to its default state, simply click the 'Unzoom Plot' button below the plot view.`,
+              image: require('../../assets/images/UnzoomPlot.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -140,23 +108,15 @@ export default {
           sections: {
             Intro: {
               id: 'browse-intro',
-              content: `
-                <div>
-                  <p>
-                    The Browse tab is used to quickly navigate through the files by plotting one curve at a time. Users can either click the arrows surrounding the file name at the left side panel or user the left and right arrow keys to plot data. The other feature in browse is the ability to tag files with unique names so that users can filter for specific files when fitting or combining data.
-                  </p>
-                </div>`,
+              info: 'The Browse tab is used to quickly navigate through the files by plotting one curve at a time. Users can either click the arrows surrounding the file name at the left side panel or user the left and right arrow keys to plot data. The other feature in browse is the ability to tag files with unique names so that users can filter for specific files when fitting or combining data.',
+              image: false,
+              imageCaption: '',
             },
             'Tagging Files': {
               id: 'browse-tagging-files',
-              content: `
-                <div>
-                  <p>
-                    To tag a file use the tag panel to in the left sidebar.
-                  </p>
-
-                  <img alt='Tagging Files Image' src='../../static/TaggingFile.gif'>
-                </div>`,
+              info: 'To tag a file use the tag panel to in the left sidebar.',
+              image: require('../../assets/images/TaggingFile.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -164,67 +124,39 @@ export default {
           sections: {
             Intro: {
               id: 'fit-intro',
-              content: `
-                <div>
-                  <p>
-                    The Graph Tab allows users to plot multiple files, as well as perform various types of curve fittings.
-                  </p>
-                </div>`,
+              info: 'The Graph Tab allows users to plot multiple files, as well as perform various types of curve fittings.',
+              image: false,
+              imageCaption: '',
             },
             'Fit Data': {
               id: 'fit-data',
-              content: `
-                <div>
-                  <p>
-                    The controls for fitting data are found in the left sidebar panel.
-                  </p>
-
-                  <img alt='Fit Data Gif' src='../../static/FitData.gif'>
-                </div>`,
+              info: 'The controls for fitting data are found in the left sidebar panel.',
+              image: require('../../assets/images/FitData.gif'),
+              imageCaption: '',
             },
             'Fit Results': {
               id: 'fit-results',
-              content: `
-                <div>
-                  <p>
-                    The results for fitting a curve will be displayed below the plot view.
-                  </p>
-
-                  <img alt='Fit Results Gif' src='../../static/FitResults.gif'>
-                </div>`,
+              info: 'The results for fitting a curve will be displayed below the plot view.',
+              image: require('../../assets/images/FitResults.gif'),
+              imageCaption: '',
             },
             'Edit Fit Equation': {
               id: 'fit-edit-equation',
-              content: `
-                <div>
-                  <p>
-                    Users can edit the original fit equation by changing the equation itself or altering the intial coefficient values. Coefficient values can be toggled constant if desired.
-                  </p>
-
-                  <img alt='Fit Equation Gif' src='../../static/FitEquation.gif'>
-                </div>`,
+              info: 'Users can edit the original fit equation by changing the equation itself or altering the intial coefficient values. Coefficient values can be toggled constant if desired.',
+              image: require('../../assets/images/FitEquation.gif'),
+              imageCaption: '',
             },
             'Point Pick': {
               id: 'fit-point-pick',
-              content: `
-                <div>
-                  <p>
-                    Some content in graph's data
-                  </p>
-
-                  <img alt='Point Pick Gif' src='../../static/PickPoints.gif'>
-                </div>`,
+              info: `Users can pick values on the plot view as the initial values of an equation's coefficient values.`,
+              image: require('../../assets/images/PickPoints.gif'),
+              imageCaption: '',
             },
             'Levenberg Settings': {
               id: 'fit-levenberg-settings',
-              content: `
-                <div>
-                  <p>
-                    Some content in graph's data
-                  </p>
-
-                  <img alt='Levenberg Settings Gif' src='../../static/LevenbergSettings.gif'>
-                </div>`,
+              info: `Users can adjust the settings of the levenberg-marquardt algorithm for fine-tune fittings.`,
+              image: require('../../assets/images/LevenbergSettings.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -232,25 +164,15 @@ export default {
           sections: {
             Intro: {
               id: 'combine-intro',
-              content: `
-                <div>
-                  <p>
-                    Some content for combine's data
-                  </p>
-
-                  <img alt='Combine Data Gif' src='../../static/CombineData.gif'>
-                </div>`,
+              info: 'Users can combine one or more curves into a single curve. The combined data can then be downloaded as a .CSV or immediately viewed in the Graph tab.',
+              image: require('../../assets/images/CombineData.gif'),
+              imageCaption: '',
             },
             'Store Combined': {
               id: 'combine-stroe',
-              content: `
-                <div>
-                  <p>
-                    Users can store combined data and plot then plot it in the graph tab.
-                  </p>
-
-                  <img alt='Store Combined Gif' src='../../static/StoreCombined.gif'>
-                </div>`,
+              info: 'Users can store combined data and plot then plot it in the graph tab.',
+              image: require('../../assets/images/StoreCombined.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -258,14 +180,9 @@ export default {
           sections: {
             Intro: {
               id: 'stitch-intro',
-              content: `
-                <div>
-                  <p>
-                    Content for stitch data's
-                  </p>
-
-                  <img alt='Stitch Data Gif' src='../../static/StitchData.gif'>
-                </div>`,
+              info: 'Users can stitch two or more curves.',
+              image: require('../../assets/images/StitchData.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -273,15 +190,9 @@ export default {
           sections: {
             Intro: {
               id: 'runs-intro',
-              content: `
-                <div>
-                  <p>
-                    Content for runs table
-                  </p>
-
-                  <img alt='Runs Table Demo Gif' src='../../static/RunsTable.gif'>
-                </div>
-              `,
+              info: 'Users (TAS and POWDER) can navigate to the Runs tab and view an all files metata. They can also search the table for specific information or exclude columns they do not need to view.',
+              image: require('../../assets/images/RunsTable.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -289,14 +200,9 @@ export default {
           sections: {
             '2D Plots': {
               id: 'sans-2d',
-              content: `
-                <div>
-                  <p>
-                    Content for 2d's plot
-                  </p>
-
-                  <img alt='SANS 2D Gif' src='../../static/SANS2D.gif'>
-                </div>`,
+              info: 'SANS users can plot 2D files.',
+              image: require('../../assets/images/SANS2D.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -304,14 +210,9 @@ export default {
           sections: {
             'Metadata Table': {
               id: 'tas-metadata',
-              content: `
-                <div>
-                  <p>
-                    Content for TAS's data
-                  </p>
-
-                  <img alt='Metadata Table Gif' src='../../static/MetadataTable.gif'>
-                </div>`,
+              info: `Users can view a file's metadata below the plot view.`,
+              image: require('../../assets/images/MetadataTable.gif'),
+              imageCaption: '',
             },
           },
         },
@@ -319,16 +220,14 @@ export default {
           sections: {
             'Metadata Table': {
               id: 'powder-metadata',
-              content: `
-                <div>
-                  <p>
-                    Same as <a href='#tas-metadata'>TAS Metadata</a>.
-                  </p>
-                </div>`,
+              info: `Same as TAS' metadata`,
+              image: false,
+              imageCaption: '',
             },
           },
         },
       },
+      /* eslint-enable */
     };
   },
 };
